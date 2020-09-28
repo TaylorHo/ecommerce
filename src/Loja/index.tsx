4a0b6263ec6994a9a7c21126a05e2fc6731038e6
Shiro Neko <tagiki5@gmail.com>
2020-09-28 18:02:02 -0300

    Estrutura base do resumo do pedido adicionada

--- src/Loja/index.tsx
+++ src/Loja/index.tsx
@@ -5,6 +5,8 @@ import ProductCategory, {produtos} from '../components/ProductCategory';
 import './styles.css';
 
 function Loja(){
+
+  const [pagamento, setPagamento] = useState('');
   
   const [classe, setClasse] = useState('inactive');
   const [icone, setIcone] = useState('up');
@@ -54,7 +56,53 @@ function Loja(){
             </div>
           </div>
           <div className={classe}>
-            <div className="detalhes-do-pedido">Detalhes do pedido</div>
+            <div className="detalhes-do-pedido">
+
+              <div className="msg-do-carrinho">
+                <span>Carrinho vazio, adicione produtos para iniciar o pedido.</span>
+              </div>
+
+              <div className="nota-do-pedido">
+                <div className="subtotal dados">
+                  <span className="dados-txt">Subtotal:</span>
+                  <span className="dados-val">R$0,00</span>
+                </div>
+                <div className="entrega dados">
+                  <span className="dados-txt">Entrega:</span>
+                  <span className="dados-val">R$0,00</span>
+                </div>
+                <div className="total dados">
+                  <span className="dados-txt">Total:</span>
+                  <span className="dados-val">R$0,00</span>
+                </div>
+              </div>
+
+              <div className="detalhes">
+                <div className="title">
+                  <span>Detalhes do Pedido</span>
+                </div>
+                <div className="msg-dos-detalhes">
+                  <span>Pedido não iniciado. Adicione um produto para iniciar.</span>
+                </div>
+                <div className="endereco">
+                  <i className="material-icons">create</i>
+                  <a href="#">&nbsp;Editar Endereço</a>
+                </div>
+                <div className="metodo-de-pagamento">
+                  <select value={pagamento} name="metodos-de-pagamento" onChange={(e) => { setPagamento(e.target.value) }}>
+                    <option value="" disabled hidden >Método de Pagamento</option>
+                    <option value="dinheiro">Dinheiro</option>
+                    <option value="credito">Cartão de Crédito</option>
+                    <option value="debito">Cartão de Débito</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="fixed-bottom">
+                <a href="#">Realizar Pedido</a>
+              </div>
+
+            </div>
           </div>
         </div>
 
