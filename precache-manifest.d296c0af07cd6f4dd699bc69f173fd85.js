eeedb78506cfef8f75ef156a0c85c4b8c3f13b35
Shiro Neko <tagiki5@gmail.com>
2020-09-29 17:47:54 -0300

    Updates

---
+++ precache-manifest.d296c0af07cd6f4dd699bc69f173fd85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579f67c891250f4af08191eabb4b3ddd",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "8c87b0fab323c72ab6a3",
+    "url": "/ecommerce/static/css/main.b75d6aae.chunk.css"
+  },
+  {
+    "revision": "d3f695f5c48e303798c7",
+    "url": "/ecommerce/static/js/2.1cc8b8e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce/static/js/2.1cc8b8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c87b0fab323c72ab6a3",
+    "url": "/ecommerce/static/js/main.4de361d1.chunk.js"
+  },
+  {
+    "revision": "05e20acd2c4e4cc9b126",
+    "url": "/ecommerce/static/js/runtime-main.23e8d565.js"
+  },
+  {
+    "revision": "3c3f5e1e0fc0b10eb30ebabcb6e32b9b",
+    "url": "/ecommerce/static/media/whatsapp.3c3f5e1e.svg"
+  }
+]);
